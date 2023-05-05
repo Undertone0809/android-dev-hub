1377bac034981db35f5ad8d144142357e2891233
Zeeland <zeeland@foxmail.com>
2023-05-05 15:09:50 +0800

    feat: add lifecycle

--- exp/LifeCycle/app/src/main/java/com/zeeland/lifecycle/MainActivity.kt
+++ exp/LifeCycle/app/src/main/java/com/zeeland/lifecycle/MainActivity.kt
@@ -1,11 +1,70 @@
 package com.zeeland.lifecycle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private val tag = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(tag, "onCreate")
         setContentView(R.layout.activity_main)
+
+        val cancelButton = findViewById<Button>(R.id.cancel)
+        val startSecondActivityButton = findViewById<Button>(R.id.startSecondActivity)
+
+        // navigate to second activity
+        startSecondActivityButton.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            Log.i(tag, "click cancel button")
+            startActivity(intent)
+        }
+
+        // exit program
+        cancelButton.setOnClickListener {
+            Log.i(tag, "click exit button")
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(tag, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(tag, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(tag, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(tag, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(tag, "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(tag, "onRestart")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val tempData = "Something you just typed"
+        outState.putString("data_key", tempData)
     }
 }
